4f344fc5faa076ec59dae382a53122432427ea07
Abdul Waqas <www.waqas7172@gmail.com>
2026-01-18 16:51:43 +0500

    add module type

--- SQL/hospital_system/backend/src/config/db.js
+++ SQL/hospital_system/backend/src/config/db.js
@@ -1,6 +1,10 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config(); 
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 
-const db = mysql.createPool({
+const db = mysql.createPool({ 
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -12,7 +16,7 @@ const db = mysql.createPool({
 try {
   const connection = await db.getConnection();
   console.log("✅ MySQL Database Connected Successfully");
-  connection.release();
+  connection.release(); 
 } catch (error) {
   console.error("❌ MySQL Connection Failed");
   console.error(error.message);
