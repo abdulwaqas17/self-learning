20660e9ff545da141d1668a802c7990b1e454a37
Abdul Waqas <www.waqas7172@gmail.com>
2026-01-20 23:58:56 +0500

    remove commited code

--- web-socket/backend/server.js
+++ web-socket/backend/server.js
@@ -69,6 +69,8 @@ const io = new Server(server, {
 // Store online users
 const onlineUsers = new Map();
 
+
+// socket code
 io.on("connection", async (socket) => {
   console.log("user connected", socket);
 
@@ -124,57 +126,6 @@ io.on("connection", async (socket) => {
   });
 });
 
-// io.on("connection", async (socket) => {
-//   console.log("User connected: socket ==>", socket, "socket id ==>", socket.id);
-
-//   // Handle user registration
-//   socket.on("register", async (username) => {
-//     onlineUsers.set(socket.id, username);
-//     io.emit("onlineUsers", Array.from(onlineUsers.values()));
-//     console.log("=================emit users===================");
-//   });
-
-//   // Send old messages
-//   socket.on("getOldMessages", async () => {
-//     try {
-//       const messages = await Message.find().sort({ timestamp: 1 }).limit(100);
-//       socket.emit("oldMessages", messages);
-//       console.log("================oldMessages get by on ====================");
-//     } catch (error) {
-//       console.error("Error fetching messages:", error);
-//       console.log("================oldMessages error====================");
-//     }
-//   });
-
-//   // Handle new messages
-//   socket.on("sendMessage", async (data) => {
-//     try {
-//       const newMsg = await Message.create({
-//         ...data,
-//         timestamp: new Date(),
-//       });
-//       io.emit("receiveMessage", newMsg);
-//     } catch (error) {
-//       console.error("Error saving message:", error);
-//     }
-//   });
-
-//   // Handle typing indicator
-//   socket.on("typing", (data) => {
-//     socket.broadcast.emit("userTyping", data);
-//   });
-
-//   socket.on("stopTyping", () => {
-//     socket.broadcast.emit("userStopTyping");
-//   });
-
-//   // Handle disconnection
-//   socket.on("disconnect", () => {
-//     onlineUsers.delete(socket.id);
-//     io.emit("onlineUsers", Array.from(onlineUsers.values()));
-//     console.log("User disconnected:", socket.id);
-//   });
-// });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
