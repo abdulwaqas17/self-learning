d535feab8e3078a91347cf28b586fdde315f8abf
Abdul Waqas <www.waqas7172@gmail.com>
2026-01-23 10:44:48 +0500

    commit changes

--- Redux Tool Kit/vite-project/src/app/store.js
+++ Redux Tool Kit/vite-project/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mobileReducer from '../features/mobile/mobileSlice';
 
+// redux store configuration
 export const store = configureStore({
   reducer: {
     mobiles: mobileReducer,
