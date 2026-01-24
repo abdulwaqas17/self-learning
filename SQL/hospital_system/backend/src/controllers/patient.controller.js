693540094b5bf483c6d615fe5fdcf8ed2f0c772c
Abdul Waqas <www.waqas7172@gmail.com>
2026-01-24 19:56:27 +0500

    add patient crud

--- SQL/hospital_system/backend/src/controllers/patient.controller.js
+++ SQL/hospital_system/backend/src/controllers/patient.controller.js
@@ -1,5 +1,11 @@
 import * as patientService from "../services/patient.service.js";
 
+console.log('=================patientService===================');
+console.log(patientService);
+console.log('=================patientService===================');
+
+
+// create patient function
 export const createPatient = async (req, res) => {
   try {
     const patientId = await patientService.createPatient(req.body);
@@ -12,10 +18,56 @@ export const createPatient = async (req, res) => {
   }
 };
 
+
+// get all patients function
 export const getAllPatients = async (req, res) => {
   try {
     const patients = await patientService.getAllPatients();
     res.status(200).json({data:patients, message: "Patients retrieved successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+// update patient function
+export const updatePatient = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updated = await patientService.updatePatient(id, req.body);
+
+    if (updated === 0) {
+      return res.status(404).json({
+        message: "Patient not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Patient updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+// delete patient function
+export const deletePatient = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleted = await patientService.deletePatient(id);
+
+    if (deleted === 0) {
+      return res.status(404).json({
+        message: "Patient not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Patient deleted successfully",
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- SQL/hospital_system/backend/src/services/patient.service.js
+++ SQL/hospital_system/backend/src/services/patient.service.js
@@ -1,5 +1,6 @@
 import db from "../config/db.js";
 
+// create patient function
 export const createPatient = async (data) => {
   const { name, age, gender, phone, address } = data;
 
@@ -12,7 +13,33 @@ export const createPatient = async (data) => {
   return result.insertId;
 };
 
+// get all patients function
 export const getAllPatients = async () => {
   const [rows] = await db.execute("SELECT * FROM patients");
   return rows;
 };
+
+// update patient function
+export const updatePatient = async (id, data) => {
+  const { name, age, gender, phone, address } = data;
+
+  const [result] = await db.execute(
+    `UPDATE patients 
+     SET name = ?, age = ?, gender = ?, phone = ?, address = ?
+     WHERE id = ?`,
+    [name, age, gender, phone, address, id]
+  );
+
+  return result.affectedRows;
+};
+
+// delete patient function
+export const deletePatient = async (id) => {
+  const [result] = await db.execute(
+    `DELETE FROM patients WHERE id = ?`,
+    [id]
+  );
+
+  return result.affectedRows;
+};
+
